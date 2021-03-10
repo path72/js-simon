f4d9dc87a2e3b46b1a38094113e567e08ba33f24
Salvatore Trimarchi <path72@gmail.com>
2021-03-10 10:09:49 +0100

    sempre rotto dal secondo giro

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -16,6 +16,10 @@ mainInit();
 // FUNCTIONS
 
 function mainInit() {
+	console.log('--------- mainInit:');
+
+	var usrSet = []; // numeri dell'utente
+	var maxN = 100; // range numerico random
 
 	$('.input_form_game').fadeIn(500);
 
@@ -27,12 +31,9 @@ function mainInit() {
 		listLength = parseInt(usrListLengthForm.val()); // numero di numeri da ricordare
 		time = parseInt(usrTimeForm.val()); // secondi di visualizzazione dei numeri
 
-		var usrSet = []; // numeri dell'utente
-		var maxN = 100; // range numerico random
-
 		// check
 		if (isNaN(listLength) || isNaN(time)) {
-			$('.msg_text').html('qualcosa non va, riprova!');
+			$('.msg_text').html('Qualcosa non va, riprova!');
 			$('.msg').fadeIn(500, function(){
 				setTimeout(function() {
 					$('.msg').fadeOut(500);
@@ -40,30 +41,36 @@ function mainInit() {
 			});
 			console.log('qualcosa non va');
 		} else {
-			gameInit(listLength,usrSet,time,maxN);
+			
+			usrSet = []; // numeri dell'utente
+
+			// random number set
+			var numSet = numSetGen(listLength,maxN);
+			console.log(numSet);
+
+			// form interactions
+			$('#usr_number_btn').click(function() { 
+				console.log('usr_number_btn >>> '+usrSet);
+				getUsrNum(numSet, usrSet); 
+			});
+			$('#usr_number').keyup(function(_ev) {
+				if (_ev.keyCode == 13) { 
+					console.log('usr_number_btn >>> '+usrSet);
+					getUsrNum(numSet, usrSet);  
+				}
+			});	
+
+			// number set showing 
+			numShow(numSet,usrSet,time);
+
 		}
 	
 	});
 
 }
 
-function gameInit(_N,_usrSet,_time,_maxN) {
-
-	// random number set
-	var numSet = numSetGen(_N,_maxN);
-
-	// form interactions
-	$('#usr_number_btn').click(function() { getUsrNum(numSet, _usrSet); });
-	$('#usr_number').keyup(function(_ev) {
-		if (_ev.keyCode == 13) { getUsrNum(numSet, _usrSet);  }
-	});	
-
-	// number set showing 
-	numShow(numSet,_usrSet,_time);
-
-}
-
 function numShow(_numSet,_usrSet,_time) {
+	console.log('--------- numShow >>> '+_usrSet);
 
 	// deploy num list
 	for (var i=0; i<_numSet.length; i++) {
@@ -77,13 +84,13 @@ function numShow(_numSet,_usrSet,_time) {
 		setTimeout(function() {
 			console.log('timer stop');
 			$('.number_display').fadeOut(500, askNum(_usrSet));
-			;
 		}, _time*1000);
 	});
 
 }
 
 function askNum(_usrSet) {
+	console.log('--------- askNum >>> '+_usrSet);
 
 	// display form
 	askMsgUpdate(_usrSet);
@@ -93,27 +100,37 @@ function askNum(_usrSet) {
 }
 
 function askMsgUpdate(_usrSet) {
+	console.log('--------- askMsgUpdate >>> '+_usrSet);
+
 	$('#usr_number_label').html('Tentativo #'+(_usrSet.length+1));
+
 }
 
 function getUsrNum(_numSet,_usrSet) {
+	console.log('--------- getUsrNum >>> '+_usrSet);
 
 	// form data retrieving
 	var usrNumberForm = $('#usr_number');
 	usrNum = parseInt(usrNumberForm.val());
+	console.log('usrNum = '+usrNum);
 
 	// data check
-	if (!_usrSet.includes(usrNum)) {
+	if (!_usrSet.includes(usrNum) && !isNaN(usrNum)) {
 		_usrSet.push(usrNum);
-		console.log(_usrSet);
+		console.log('caso 1: '+_usrSet);
 	} else {
-		$('.msg_text').html(usrNum+' è già presente, riprova!');
+		if (isNaN(usrNum)) {
+			$('.msg_text').html(usrNum+' non è un numero, riprova!');
+			console.log('caso 2: '+usrNum+' non è un numero, riprova!');
+		} else {
+			$('.msg_text').html(usrNum+' è già presente, riprova!');
+			console.log('caso 3: '+usrNum+' è già presente, riprova!');
+		}
 		$('.msg').fadeIn(500, function(){
 			setTimeout(function() {
 				$('.msg').fadeOut(500);
 			}, 1000);
 		});
-		console.log(usrNum+' presente');
 	}
 
 	// form cleaning
@@ -129,16 +146,19 @@ function getUsrNum(_numSet,_usrSet) {
 }
 
 function getResult(_numSet,_usrSet) {
+	console.log('--------- getResult >>> '+_usrSet);
 
 	// right number collection 
 	var areIn = [];
 	for (var i=0; i<_usrSet.length; i++) {
 		if (_numSet.includes(_usrSet[i])) areIn.push(_usrSet[i]);
 	}
-	console.log('areIn: '+areIn);
+	console.log('areIn = '+areIn);
 
 	// final message
 	finalMsg(_numSet,areIn);
+
+	// _usrSet = [];
 
 }
 
@@ -158,10 +178,14 @@ function finalMsg(_numSet,_areIn) {
 	$('.result_list').append(msg);
 	$('.result').fadeIn(500, function() {
 		setTimeout(function() {
+
+			// reset game
 			$('.result').fadeOut(500);
 			$('#usr_list_length').val('');
 			$('#usr_time').val('');
 			$('.num_list').html('');
+			// mainInit();
+
 		}, 4000);
 	});
 
@@ -174,8 +198,9 @@ function finalMsg(_numSet,_areIn) {
 
 function numSetGen(_N,_maxN) {
 	var numSet = [];
-	for (var i=0; i<_N; i++) {
-		numSet.push(randomNumber(1,_maxN));
+	while (numSet.length<_N) {
+		var r = randomNumber(1,_maxN);
+		if (!numSet.includes(r)) numSet.push(r);
 	}
 	return numSet;
 }
